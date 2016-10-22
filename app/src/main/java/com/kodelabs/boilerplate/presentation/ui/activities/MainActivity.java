ed627382ffe6bf6f3182e09cdacc6b5de454bb67
Ricardo Amaro <ricardoamaro@live.com.pt>
2016-10-22 15:42:15 +0100

    MainActivity class - override of the onResume method

--- app/src/main/java/com/kodelabs/boilerplate/presentation/ui/activities/MainActivity.java
+++ app/src/main/java/com/kodelabs/boilerplate/presentation/ui/activities/MainActivity.java
@@ -4,14 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.kodelabs.boilerplate.R;
+import com.kodelabs.boilerplate.presentation.presenters.MainPresenter;
 import com.kodelabs.boilerplate.presentation.presenters.MainPresenter.View;
 
 public class MainActivity extends AppCompatActivity implements View {
+
+    private MainPresenter mainPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // let's start welcome message retrieval when the app resumes
+        mPresenter.resume();
     }
 
     @Override
